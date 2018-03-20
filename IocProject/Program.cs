900b312583cc785418b18d708a74d8f0d822aba3
Adam C <a.clay0x2a@gmail.com>
2018-03-20 17:20:45 -0600

    Phase 1 & Phase 2 Assignments
    Get assignments for Phase 1 and Phase 2 of the IoC project

--- IocProject/Program.cs
+++ IocProject/Program.cs
@@ -36,6 +36,12 @@ namespace IocProject
     {
       var assembly = Assembly.GetExecutingAssembly();
       var userType = assembly.GetType("IocProject.TestUser");
+      var ctorArgs = userType.GetConstructors().First().GetParameters();
+      // Phase 1: Get this list of types from the constructor, and then build a list of default values for primitive types, nulls for non-primitive for the given constructor
+      // Then execute to create an object
+
+      // Phase 2: Create a Dictionary of registered concrete types to construct default values for non-primitive types using recursion to identify parameters to get its own values
+
       var ctor = userType.GetConstructor(new Type[] { typeof(string), typeof(string) });
       var userInstance = ctor.Invoke(new object[] { "aclay", "test123" });
       var loginMethod = userType.GetMethod("Login");
